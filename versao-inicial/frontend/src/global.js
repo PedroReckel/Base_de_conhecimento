dc03c7dc5999d07dac6f39c5ee14443833bf7255
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2023-07-18 17:45:04 -0300

    Projeto Base de Conhecimento - Frontend

--- versao-inicial/frontend/src/global.js
+++ versao-inicial/frontend/src/global.js
@@ -1,1 +1,17 @@
+import Vue from 'vue'
+
 export const baseApiUrl = 'http://localhost:3000'
+
+// Cenarios comuns aonde se encontram os erros
+export function showError(e) {
+    if(e && e.response && e.response.data) {
+        Vue.toasted.global.defaultError({ msg : e.response.data })
+    } else if(typeof e === 'string') {
+        Vue.toasted.global.defaultError({ msg : e })
+    } else {
+        Vue.toasted.global.defaultError()
+    }
+}
+
+// Também dá para exportar um objeto contendo todos os elementos
+// export default { baseApiUrl, showError }
--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -3,13 +3,14 @@ import Vue from 'vue'
 
 import App from './App'
 import './config/bootstrap'
+import './config/msgs'
 import store from './config/store'
 import router from './config/router'
 
 Vue.config.productionTip = false
 
 // TEMPORARIO! // Para qualquer requisição que o axios fizer automaticamente terá um header chamado autorization 
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NSwibmFtZSI6IlBlZHJvIFJlY2tlbCBSb2JlcnRlIiwiZW1haWwiOiJwZWRyb3JlY2tlbDkwQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2ODkyOTU0NjQsImV4cCI6MTY4OTMxNzA2NH0.dH9ir54OdhozHMVwr847QaXQex_pZk4DxVeIe2k6p4c'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NSwibmFtZSI6IlBlZHJvIFJlY2tlbCBSb2JlcnRlIiwiZW1haWwiOiJwZWRyb3JlY2tlbDkwQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2ODk2OTc4OTQsImV4cCI6MTY4OTcxOTQ5NH0.l4w_Yb9Kfl2NL-TqDQ6cVFAJtpsBlOgpMnQfAwnajys'
 
 new Vue({
   store,
