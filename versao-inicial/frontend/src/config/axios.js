e4d9633dfc49220a1c14552575fbb8f03a212b6c
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2023-08-10 21:24:39 -0300

    Corrigindo funcionamento do Vue Tostaded

--- versao-inicial/frontend/src/config/axios.js
+++ versao-inicial/frontend/src/config/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // Caso o token esteja expirado o usuário será levado para a página raiz da aplicação 
 const success = res => res
 const error = err => {
-    if(401 === err.reponse.status) {
+    if(401 === err.response.status) {
         window.location = '/'
     } else {
         return Promise.reject(err) 
